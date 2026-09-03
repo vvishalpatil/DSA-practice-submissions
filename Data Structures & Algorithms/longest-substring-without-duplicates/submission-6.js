f140006824b73c0ff1f4c1887cc952bc85c0a046
vvishalpatil <35795030+vvishalpatil@users.noreply.github.com>
2026-09-03 15:17:10 +0530

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let max = 0
+        let map = {}
+        let start = 0
+        if(s.length<=1){
+            return s.length
+        }
+        for(let i = 0;i<s.length;i++){
+            let ch = s[i]
+            if(map[ch]!==undefined){
+                start = Math.max(start, map[ch]+1)
+            }
+            map[ch]=i
+            max = Math.max(max,i-start+1)
+
+        }
+        return max
+    }
+}
